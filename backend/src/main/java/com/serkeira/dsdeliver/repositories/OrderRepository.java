4ab8d5fb3832564ff6a48cd2186658acf810000f
Lucas C <61256306+serkeira@users.noreply.github.com>
2021-01-09 20:07:31 -0300

    GET /orders (filtrando por pending)

--- backend/src/main/java/com/serkeira/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/serkeira/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,19 @@
 package com.serkeira.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.serkeira.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	// deve ser exatamente igual o nome da CLASSE 
+	// o obj.products se refere a própria lista que se encontra na classe
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC ")
+	List<Order> findOrdersWithProducts();
+	
 
 }
