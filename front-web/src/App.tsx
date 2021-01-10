33a86996d12c374b84a6f34d0c74d311bc0b9358
Lucas C <61256306+serkeira@users.noreply.github.com>
2021-01-10 11:30:44 -0300

    Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,13 @@
-import "./App.css";
+import Home from "./Home";
 import Navbar from "./Navbar";
-
-
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
