4e623d91a898be6e11d64d4f51a251d4c4735686
Lucas C <61256306+serkeira@users.noreply.github.com>
2021-01-09 22:03:17 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/serkeira/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/serkeira/dsdeliver/services/OrderService.java
@@ -21,30 +21,37 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
-		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); 
-		
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+
 	}
 	
 	@Transactional
-	public OrderDTO insert(OrderDTO dto) {
-		 Order order = new Order(null, dto.getAddress(), dto.getLatitude(), 
-				 dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
-		
-		 for (ProductDTO p : dto.getProducts()) {
-			 Product product = productRepository.getOne(p.getId());
-			 order.getProducts().add(product);
-		 }
-		 order = repository.save(order);
-		 return new OrderDTO(order);
-		 
-		 
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
