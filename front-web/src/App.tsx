6f74943430f5996e48310e3bb9b8f6b7f1d277ab
Lucas C <61256306+serkeira@users.noreply.github.com>
2021-01-10 10:24:09 -0300

    Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
